e643c27efdfddbfa300297b4edc6d4102f668832
Xiaolei Huang <xiaoleihuangccnu@gmail.com>
2021-12-04 12:20:59 -0600

    add new baseline

--- baseline/adversarial.py
+++ baseline/adversarial.py
@@ -1,3 +1,11 @@
+"""Partial Codes adopt from Decoupling adversarial training for fair NLP
+https://aclanthology.org/2021.findings-acl.41.pdf
+
+model also follow the same idea as The Authors Matter:
+Understanding and Mitigating Implicit Bias in Deep Text Classification
+https://arxiv.org/pdf/2105.02778.pdf
+
+"""
 import os
 import argparse
 import datetime
@@ -5,15 +13,138 @@ import datetime
 import numpy as np
 from sklearn import metrics
 from tqdm import tqdm
+from imblearn.over_sampling import RandomOverSampler
 
 import torch
+import torch.nn as nn
 from torch.utils.data import DataLoader
-from imblearn.over_sampling import RandomOverSampler
-from transformers import BertForSequenceClassification
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 import utils
 
 
-def build_bert(params):
+class DeepMojiModel(nn.Module):
+    def __init__(self, params):
+        super(DeepMojiModel, self).__init__()
+        self.params = params
+        self.emb_size = self.params['emb_size']
+        self.hidden_size = self.params['hidden_size']
+        self.num_classes = self.params['num_label']
+        self.adv_level = self.params['adv_level']
+        self.n_hidden = self.params['n_hidden']
+        self.device = self.params['device']
+        self.dropout = nn.Dropout(p=self.params['dp_rate'])
+
+        self.tanh = nn.Tanh()
+        self.ReLU = nn.ReLU()
+        self.AF = nn.Tanh()
+        if self.params.get('AF', "") == "relu":
+            self.AF = self.ReLU
+        self.dense1 = nn.Linear(self.emb_size, self.hidden_size)
+        self.dense2 = [nn.Linear(self.hidden_size, self.hidden_size).to(self.device) for _ in range(self.n_hidden)]
+        self.dense3 = nn.Linear(self.hidden_size, self.num_label)
+
+    def forward(self, inputs):
+        out = self.dense1(inputs)
+        out = self.AF(out)
+        for hl in self.dense2:
+            out = self.dropout(out)
+            out = hl(out)
+            out = self.tanh(out)
+        out = self.dense3(out)
+        return out
+
+    def hidden(self, inputs):
+        assert self.adv_level in {0, -1, -2}
+        out = self.dense1(inputs)
+        out = self.AF(out)
+        if self.adv_level == -2:
+            return out
+        else:
+            for hl in self.dense2:
+                out = self.dropout(out)
+                out = hl(out)
+                out = self.tanh(out)
+            if self.adv_level == -1:
+                return out
+            else:
+                out = self.dense3(out)
+                return out
+
+
+class GradientReversalFunction(torch.autograd.Function):
+    """
+    From:
+    https://github.com/jvanvugt/pytorch-domain-adaptation/blob/cb65581f20b71ff9883dd2435b2275a1fd4b90df/utils.py#L26
+    Gradient Reversal Layer from:
+    Unsupervised Domain Adaptation by Backpropagation (Ganin & Lempitsky, 2015)
+    Forward pass is the identity function. In the backward pass,
+    the upstream gradients are multiplied by -lambda (i.e. gradient is reversed)
+    """
+
+    @staticmethod
+    def forward(ctx, x, lambda_):
+        ctx.lambda_ = lambda_
+        return x.clone()
+
+    @staticmethod
+    def backward(ctx, grads):
+        lambda_ = ctx.lambda_
+        lambda_ = grads.new_tensor(lambda_)
+        dx = -lambda_ * grads
+        return dx, None
+
+
+class GradientReversal(torch.nn.Module):
+    def __init__(self, lambda_):
+        super(GradientReversal, self).__init__()
+        self.lambda_ = lambda_
+
+    def forward(self, x):
+        return GradientReversalFunction.apply(x, self.lambda_)
+
+
+class Discriminator(nn.Module):
+    def __init__(self, params):
+        super(Discriminator, self).__init__()
+
+        self.GR = False
+        self.grad_rev = GradientReversal(params['LAMBDA'])
+        self.fc1 = nn.Linear(params['input_size'], params['adv_units'])
+        self.LeakyReLU = nn.LeakyReLU()
+        self.fc2 = nn.Linear(params['adv_units'], params['adv_units'])
+        self.fc3 = nn.Linear(params['adv_units'], len(params['unique_domains']))
+
+    def forward(self, inputs):
+        if self.GR:
+            inputs = self.grad_rev(inputs)
+        out = self.fc1(inputs)
+        out = self.LeakyReLU(out)
+        out = self.fc2(out)
+        out = self.fc3(out)
+        return out
+
+    def hidden_representation(self, inputs):
+        if self.GR:
+            inputs = self.grad_rev(inputs)
+        out = self.fc1(inputs)
+        out = self.LeakyReLU(out)
+        out = self.fc2(out)
+        # out = self.fc3(out)
+        # Return the hidden representation from the second last layer
+        return out
+
+    def change_gradient_eversal(self, State=True):
+        self.GR = State
+
+    def get_weights(self):
+        # return coef as numpy array
+        dense_parameter = {name: param for name, param in self.fc3.named_parameters()}
+        # get coef and covert to numpy
+        # return dense_parameter["weight"].cpu().numpy()
+        return dense_parameter["weight"].detach().cpu().numpy()
+
+
+def build_base(params):
     if torch.cuda.is_available() and params['device'] != 'cpu':
         device = torch.device(params['device'])
     else:
@@ -81,11 +212,15 @@ def build_bert(params):
     )
 
     # build model
-    bert_model = BertForSequenceClassification.from_pretrained(params['bert_name'])
-    bert_model = bert_model.to(device)
-    # param_optimizer = list(bert_model.named_parameters())
-    # no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
-    optimizer = torch.optim.Adam(bert_model.parameters(), lr=params['lr'])
+    base_model = DeepMojiModel(params)
+    base_model = base_model.to(device)
+    discriminator = Discriminator(params)
+    discriminator = discriminator.to(device)
+    optimizer = torch.optim.Adam(base_model.parameters(), lr=params['lr'])
+    adv_optimizer = torch.optim.Adam(filter(
+        lambda p: p.requires_grad, discriminator.parameters()), lr=1e-1 * params['lr'])
+    scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=0.5, patience=2)
+    criterion = nn.CrossEntropyLoss().to(device)
 
     # train the networks
     print('Start to train...')
@@ -93,18 +228,23 @@ def build_bert(params):
     best_score = 0.
     for epoch in tqdm(range(params['epochs'])):
         train_loss = 0
-        bert_model.train()
-        bert_model.change_mode('train')
+        base_model.train()
+        discriminator.train()
+        discriminator.GR = True
 
         for step, train_batch in enumerate(train_data_loader):
             train_batch = tuple(t.to(device) for t in train_batch)
-            input_docs, input_labels, _ = train_batch
+            input_docs, input_labels, input_domains = train_batch
             optimizer.zero_grad()
-            predictions = bert_model(**{
-                'input_ids': input_docs,
-            }, labels=input_labels)
-            loss = predictions.loss
+            adv_optimizer.zero_grad()
+
+            predictions = base_model(input_docs)
+            loss = criterion(predictions, input_labels)
             train_loss += loss.item()
+
+            hs = base_model.hidden(input_docs)
+            adv_predictions = discriminator(hs)
+            loss += criterion(adv_predictions, input_domains)
 
             loss_avg = train_loss / (step + 1)
             if (step + 1) % 101 == 0:
@@ -116,27 +256,34 @@ def build_bert(params):
             # torch.nn.utils.clip_grad_norm_(rnn_model.parameters(), 0.5)
             optimizer.step()
 
+            torch.nn.utils.clip_grad_norm(discriminator.parameters(), params['clipping_value'])
+            adv_optimizer.step()
+
         # evaluate on the valid set
         y_preds = []
         y_trues = []
-        bert_model.change_mode('valid')
+        discriminator.GR = False
+        base_model.eval()
+        discriminator.eval()
+        valid_loss = 0.0
         for valid_batch in valid_data_loader:
             valid_batch = tuple(t.to(device) for t in valid_batch)
             input_docs, input_labels, input_domains = valid_batch
             with torch.no_grad():
-                predictions = bert_model(**{
-                    'input_docs': input_docs,
-                    'input_domains': input_domains
-                })
+                predictions = base_model(input_docs)
+            valid_loss_batch = criterion(predictions, input_labels)
+            valid_loss += valid_loss_batch.item()
+
             logits = torch.sigmoid(predictions.detach().cpu()).numpy()
             pred_flat = np.argmax(logits, axis=1).flatten()
             y_preds.extend(pred_flat)
             y_trues.extend(input_labels.to('cpu').numpy())
+        scheduler.step(valid_loss / len(valid_data_loader))
 
         eval_score = metrics.f1_score(y_pred=y_preds, y_true=y_trues, average='weighted')
         if eval_score > best_score:
             best_score = eval_score
-            torch.save(bert_model, params['model_dir'] + '{}.pth'.format(os.path.basename(__file__)))
+            torch.save(base_model, params['model_dir'] + '{}.pth'.format(os.path.basename(__file__)))
 
             y_preds = []
             y_probs = []
@@ -148,10 +295,7 @@ def build_bert(params):
                 input_docs, input_labels, input_domains = test_batch
 
                 with torch.no_grad():
-                    predictions = bert_model(**{
-                        'input_docs': input_docs,
-                        'input_domains': input_domains
-                    })
+                    predictions = base_model(input_docs)
                 logits = torch.sigmoid(predictions.detach().cpu()).numpy()
                 pred_flat = np.argmax(logits, axis=1).flatten()
                 y_preds.extend(pred_flat)
@@ -184,7 +328,6 @@ def build_bert(params):
 
                 wfile.write('...............................\n\n')
                 wfile.flush()
-    pass
 
 
 if __name__ == '__main__':
@@ -192,7 +335,6 @@ if __name__ == '__main__':
     parser.add_argument('--lr', type=float, help='Learning rate', default=.0001)
     parser.add_argument('--batch_size', type=int, help='Batch size', default=16)
     parser.add_argument('--max_len', type=int, help='Max length', default=512)
-    parser.add_argument('--lambdaV', type=float, help='lambda_v', default=1)
     parser.add_argument('--device', type=str, default='cpu')
     args = parser.parse_args()
 
@@ -245,18 +387,20 @@ if __name__ == '__main__':
             'max_len': args.max_len,
             'dp_rate': .2,
             'optimizer': 'adamw',
-            'bert_name': 'bert-base-uncased',  # vinai/bertweet-base
             'emb_dim': 200,
             'unique_domains': [],
             'bidirectional': False,
             'device': args.device,
             'num_label': 2,
             'kl_score': 0.01,
-            'lambda_v': args.lambdaV
+            'n_hidden': 1,  # default as original paper
+            'adv_level': -1,
+            'adv_units': 256,
+            'clipping_value': 1,
         }
 
         # adjust parameters for other languages
         if parameters['lang'] != 'english':
             parameters['bert_name'] = 'bert-base-multilingual-uncased'
 
-        build_bert(parameters)
+        build_base(parameters)
